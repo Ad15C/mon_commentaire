0ae201ec1e0009a9bacd3e6aae7dffdf65ed80f9
AdelineCanon <Ad15canon@gmail.com>
2024-02-20 13:47:36 +0100

    correction_2

--- script.js
+++ script.js
@@ -12,39 +12,33 @@
     const newCommentP = newComment.querySelector(".prose, .prose-sm, .mt-4, .max-w-none, .text-gray-500");
   
 
-//Fonction ClearForm
-function clearForm () {
-   document.getElementById('first-name').value = '';
-   document.getElementById ('last-name').value ='';
-   document.getElementById('message').value = '';
+//Function myFunction
+function myFunction () {
+    document.getElementById('form').reset();
 };
 
 //soumission du form
 formulaire.addEventListener("submit", (event) => {
     event.preventDefault();
-        
+    
 //On prend les valeurs
     const firstName = document.getElementById('first-name').value.trim();
     const lastName = document.getElementById('last-name').value.trim();
     const message = document.getElementById('message').value.trim(); 
     
 //On vérifie si les champs sont vides
-    if (!firstName || !lastName || !message ) {
-//s'ils sont vides
-    errorMessage.style.display = 'block';    
+    if (!firstName|| !lastName|| !message ) {
+    errorMessage.style.display = 'block';
     console.log("un ou plusieurs champs ne sont pas correctement remplis");
     } else {
-        const copie = newComment (
-            newCommentH3,
-            newCommentP,
-        );
-    
-    commentList.appendChild(newComment);
-
-    clearForm();
     errorMessage.style.display = 'none';
-    console.log("tous les champs sont correctement remplis");
-    }
+    newCommentH3.textContent = `${firstName} ${lastName}`;
+    newCommentP.textContent = message;
+    
+// ajout le clone dans le dom
+    commentList.appendChild(newComment);
+//On réinitialise le formulaire   
+    myFunction();
 });
     
 
